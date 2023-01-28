d254b7f3eb5225c72abc365c3ac93c4d53af0c0f
prabaharanbalaji16 <prabaharanbalaji16@gmail.com>
2023-01-28 14:12:22 +0530

    first commit

---
+++ 1480. Running Sum of 1d Array/Running_Sum_of_1d_Array.java
@@ -0,0 +1,21 @@
+public class Running_Sum_of_1d_Array
+{
+    public static void main(String[] args) {
+        Running_Sum_of_1d_Array runningSum = new Running_Sum_of_1d_Array();
+        int[] num = new int[]{1,7,3,6,5,6};
+        int[] arr = runningSum.runningSum(num);
+        for(int a:arr)
+        {
+            System.out.print(a+" ");
+        }
+        System.out.println();
+    }
+
+    public int[] runningSum(int[] nums) {
+        for(int i=1;i<nums.length;i++)
+        {
+            nums[i] = nums[i]+nums[i-1];
+        }
+        return nums;
+    }
+}
