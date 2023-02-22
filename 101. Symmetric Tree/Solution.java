86832048207cfd99974fb2a1075eca92f7454633
prabaharanbalaji16 <prabaharanbalaji16@gmail.com>
2023-02-22 23:50:15 +0530

    Leetcode problems

---
+++ 101. Symmetric Tree/Solution.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    boolean flag;
+    public boolean isSymmetric(TreeNode root) {
+        flag = true;
+        helper(root.left,root.right);
+        return flag;
+    }
+    void helper(TreeNode root1,TreeNode root2)
+    {
+        if(root1==null && root2==null)
+        {
+            return;
+        }
+        if(root1==null || root2==null || root1.val!=root2.val)
+        {
+            flag= false;
+            return;
+        }
+        helper(root1.left,root2.right);
+        helper(root1.right,root2.left);
+    }
+}
+
+class TreeNode{
+    TreeNode left;
+    TreeNode right;
+    int val;
+}
---
+++ 94. Binary Tree Inorder Traversal/Solution.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        helper(root,list);
+        return list;
+    }
+
+    void helper(TreeNode root, List<Integer> list)
+    {
+        if(root==null) return;
+        helper(root.left,list);
+        list.add(root.val);
+        helper(root.right,list);
+    }
+}
+
+class TreeNode{
+    TreeNode left;
+    TreeNode right;
+    int val;
+}
