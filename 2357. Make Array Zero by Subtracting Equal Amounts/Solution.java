03bdb06b46d31f758dbb9c872bbee81b6dba5224
prabaharanbalaji16 <prabaharanbalaji16@gmail.com>
2023-03-07 23:30:44 +0530

    Leetcode problems

---
+++ 2357. Make Array Zero by Subtracting Equal Amounts/Solution.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+class Solution {
+    public int minimumOperations(int[] nums) {
+        HashSet<Integer> set=new HashSet<Integer>();
+        for(int n:nums)
+        {
+            if (n!=0)
+            {
+                set.add(n);
+            }
+        }
+        return set.size();
+    }
+}
