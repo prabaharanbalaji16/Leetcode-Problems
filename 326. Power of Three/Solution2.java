7fd36822e7b53abd2a4f28c58beb8b6b5c4b9592
prabaharanbalaji16 <prabaharanbalaji16@gmail.com>
2023-04-27 23:37:13 +0530

    Leetcode problems

---
+++ 326. Power of Three/Solution2.java
@@ -0,0 +1,7 @@
+class Solution2 {
+    public boolean isPowerOfThree(int n) {
+        if(n==1) return true;
+        else if(n%3!=0 || n<=0) return false;
+        return isPowerOfThree(n/3);
+    }
+}
