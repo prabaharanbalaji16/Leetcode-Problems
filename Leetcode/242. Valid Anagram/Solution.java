0d113d10a6b0b24cdc96e82f559d7a597aaeb0f1
prabaharanbalaji16 <prabaharanbalaji16@gmail.com>
2023-10-01 23:20:10 +0530

    leetcode problems

---
+++ Leetcode/242. Valid Anagram/Solution.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        HashMap<Character,Integer> map1= new HashMap<>();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char c1 = s.charAt(i);
+            map1.put(c1,map1.getOrDefault(c1,0)+1);
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            char c = t.charAt(i);
+            // boolean containskey = map1.containsKey(c);
+            if(!map1.containsKey(c))
+                return false;
+            else if(map1.get(c)<=0)
+                return false;
+            else 
+                map1.put(c,map1.get(c)-1);
+
+        }
+        return true;
+    }
+}
