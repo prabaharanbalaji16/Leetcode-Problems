091e2044535ddf2693996e1426b2d8973a0685f1
prabaharanbalaji16 <code.development16@gmail.com>
2023-11-13 07:32:42 +0530

    leetcode problems

--- Leetcode/637. Average of Levels in Binary Tree/Solution.java
+++ Leetcode/637. Average of Levels in Binary Tree/Solution.java
@@ -1,1 +1,38 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> list = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            Double sum=0.0;
+            for(int i=0;i<size;i++)
+            {
+                TreeNode temp = q.remove();
+                sum+=temp.val;
+                if(temp.left!=null) q.add(temp.left);
+                if(temp.right!=null) q.add(temp.right);
+            }
+            Double d = (double)sum/size;
+            list.add(d);
+        }
+        return list;
+    }
+}
