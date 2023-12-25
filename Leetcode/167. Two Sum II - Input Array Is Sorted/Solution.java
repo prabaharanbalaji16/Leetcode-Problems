57fef2dc735e1a51bf61b8411bb6a35e6b2ff246
Prabaharan Balaji <70196060+prabaharanbalaji16@users.noreply.github.com>
2023-12-25 23:02:55 +0530

    Leetcode problems

--- Leetcode/167. Two Sum II - Input Array Is Sorted/Solution.java
+++ Leetcode/167. Two Sum II - Input Array Is Sorted/Solution.java
@@ -1,47 +1,18 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-
-    // Using Binary Search
-
-
-        // int i=0,j=0, n = numbers.length-1;
-        // while(i<n)
-        // {
-        //    j= search(numbers,i+1,n,target-numbers[i]);
-        //    if(j>0)
-        //    {
-        //        break;
-        //    }
-        //     i++;
-        // }
-        // return new int[]{i+1,j+1};
-
-    // Using Two Pointer
         int i=0,j=numbers.length-1;
+        int sum = 0;
         while(i<j)
         {
-            int sum = numbers[i]+numbers[j];
+            sum = numbers[i]+numbers[j];
 
             if(sum==target)
                 return new int[]{i+1,j+1};
-            else if(sum > target) 
-                j--;
-            else
+            else if( sum < target)
                 i++;
+            else 
+                j--;
         }
-        return new int[2];
-    }
-
-    public int search(int[] arr,int low,int high, int data)
-    {
-        int mid = (low+high)/2;
-        if(low > high)
-            return -1;
-        else if(arr[mid]==data)
-            return mid;
-        else if(arr[mid]<data)
-            return search(arr,mid+1,high,data);
-        else 
-            return search(arr,low,mid-1,data);
+        return new int[]{i+1,j+1};
     }
 }
